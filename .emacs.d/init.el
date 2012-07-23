bb58abfa66edd76e50c08abb096062698bb9f019
Max Veytsman <maxim@ontoillogical.com>
2012-07-22 23:07:11 -0400

    made emacs init file more modular (now with platform detection)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,75 +1,84 @@
+; == Initialization == 
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
-
 (when (not package-archive-contents)
   (package-refresh-contents))
-
-;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-ruby starter-kit-js starter-kit-eshell starter-kit-bindings color-theme)
+(defvar my-packages '(starter-kit starter-kit-ruby starter-kit-js starter-kit-eshell starter-kit-bindings color-theme coffee-mode find-file-in-project idle-highlight-mode ido-ubiquitous inf-ruby magit markdown-mode paredit quack rvm smex)
   "A list of packages to ensure are installed at launch.")
-
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "2e130c0ce1b485fde7ba3b65ebc68cdd883ef6a7" "810a580e75aca47fc70e40f2adbebbcd27b978f9" "0174d99a8f1fdc506fa54403317072982656f127" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
-;; used to be maxim.el
-;; fonts
-;Doesn't work on windows :(
-;(set-face-attribute 'default nil :font "Deja Vu Sans Mono-12")
-;(set-default-font "-outline-Consolas-normal-r-normal-normal-14-97-96-96-c-*-iso8859-1")
-
-;; the visual bell is broken on macs in emacs 23
-(setq ring-bell-function 'ignore)
-
-;; add texbin to path
-;;(setenv "PATH" (concat (getenv "PATH") ":/usr/texbin" ":/usr/local/bin"))
-;;(setq exec-path (append exec-path '("/usr/texbin" "/usr/local/bin")))
-;;(require 'tex-site)
-; I prefer M-tab for hippie expand
-(global-set-key (read-kbd-macro "<M-tab>") 'hippie-expand)
-
-;; smex is like ido but for M-x
+;; package specific initialization 
 (smex-initialize)
+(rvm-use-default)
 
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; OS X breaks some things
+(if (eq system-type 'darwin)
+    (progn
+      ;ispell is broken
+      (setq-default ispell-program-name "/usr/local/bin/aspell")
+      ;visual bell looks crappy
+      (setq visible-bell nil)))
 
-
-(global-set-key (kbd "C-C-h") 'backward-kill-word)
-
-;; Colors!
-
-;; major modes
-(setq c-default-style "linux"
-      c-basic-offset 4)
-;; org stuff
-(setq org-startup-indent 0
-      org-hide-leading-stars 1
-      org-startup-folded 0)
-
-;;make scripts executable
- (add-hook 'after-save-hook
-    'executable-make-buffer-file-executable-if-script-p)
-
-;; ispell broken on OS X
-(setq-default ispell-program-name "/usr/local/bin/aspell")
+; == Themes and Fonts ==
+(custom-set-variables
+;; custom-set-variables was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("723d8e038750a51aa9d6f1000a6f5047f343a10291a07dfb30c8f35fa9bfe8ec" "810a580e75aca47fc70e40f2adbebbcd27b978f9" "0174d99a8f1fdc506fa54403317072982656f127" default))))
+(custom-set-faces
+;; custom-set-faces was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+ )
+;; Default font choice --- Consolas on windows, otherwise Inconsolata
+(if (eq system-type 'windows-nt)
+    (set-default-font "-outline-Consolas-normal-r-normal-normal-14-97-96-96-c-*-iso8859-1")
+  (set-face-attribute 'default nil :font "Inconsolata Sans Mono-14"))
 
 ;; solarize theme
 (add-to-list 'load-path "~/.emacs.d/emacs-color-theme-solarized")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized")
 (load-theme 'solarized-dark)
+ 
+; == Key Bindings ==
+;; I prefer M-tab for hippie expand
+(global-set-key (read-kbd-macro "<M-tab>") 'hippie-expand)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-C-h") 'backward-kill-word)
+;;in OS X enable fullscrean lion support with Apple-shift-f
+(if (eq system-type 'darwin)
+  (progn
+    (global-set-key (kbd "<s-return>") 'ns-toggle-fullscreen)
+    (global-set-key (kbd "<C-return>") 'ns-toggle-fullscreen)))
+
+; == Major Modes and Tabs ==
+;;4 space tabs
+(setq-default tab-width 4)
+(setq c-default-style "linux"
+      c-basic-offset 4)
+;;make scripts executable
+ (add-hook 'after-save-hook
+    'executable-make-buffer-file-executable-if-script-p)
+;; org stuff
+(setq org-startup-indent 0
+      org-hide-leading-stars 1
+      org-startup-folded 0)
+
+;;idiomatic coffee-script is 2 tabs per space
+(defun coffee-custom ()
+  "coffee-mode-hook"
+ (set (make-local-variable 'tab-width) 2))
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
+
+;;racket mode
+(require 'quack)
+(load-file "~/.emacs.d/packages/geiser/elisp/geiser.el")
+(setq geiser-racket-binary "/Applications/Racket v5.2.1/bin/racket")
 
